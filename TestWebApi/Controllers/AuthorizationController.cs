f962d048420a094edf8d2cadedddfa8f1dce1541
Terence Golla <tgolla@outlook.com>
2023-04-17 17:00:53 -0500

    Authorization test completed.

--- TestWebApi/Controllers/AuthorizationController.cs
+++ TestWebApi/Controllers/AuthorizationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TGolla.AspNetCore.Mvc.Filters;
 using TGolla.Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace TestWebApi.Controllers
@@ -13,11 +14,44 @@ namespace TestWebApi.Controllers
     public class AuthorizationController : Controller
     {
         /// <summary>
-        /// 
+        /// Test for an authenticated user. No group privileges needed.
         /// </summary>
         /// <returns></returns>
-        [HttpGet("Test")]
+        [HttpGet("TestForAnAuthenticatedUser")]
+        public IActionResult TestForAnAuthenticatedUser() { return Ok(); }
+
+        /// <summary>
+        /// Test for a user with administrator privileges.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("TestForAnAdministrator")]
+        [Authorize(Policy = "Administrator")]
+        public IActionResult TestForAnAdministrator() { return Ok(); }
+
+        /// <summary>
+        /// Test for a user with manager privileges.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("TestForAManager")]
         [Authorize(Policy = "Manager")]
-        public IActionResult Test() { return Ok(); }
+        public IActionResult TestForAManager() { return Ok(); }
+
+        /// <summary>
+        /// Test for a user with manager and administrator privileges.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("TestForAManagerAdministrator")]
+        [Authorize(Policy = "Manager")]
+        [Authorize(Policy = "Administrator")]
+        public IActionResult TestForAManagerAdministrator() { return Ok(); }
+
+        /// <summary>
+        /// Test for a user with either manager and/or administrator privileges.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("TestForEitherAManagerOrAdministrator")]
+        [AuthorizeOnAnyOnePolicy("Manager,Administrator")]
+        public IActionResult TestForEitherAManagerOrAdministrator() { return Ok(); }
+
     }
 }
