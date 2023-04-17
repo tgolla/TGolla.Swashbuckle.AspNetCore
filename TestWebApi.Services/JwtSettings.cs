4167faf00d7e6ab63d89a94d0b3f6f60e1af8415
Terence Golla <tgolla@outlook.com>
2023-04-17 10:59:26 -0500

    Added JWT settings.

---
+++ TestWebApi.Services/JwtSettings.cs
@@ -0,0 +1,38 @@
+﻿namespace TestWebApi.Services
+{ 
+    /// <summary>
+    /// 
+    /// </summary>
+    public class JwtSettings
+    {
+        /// <summary>
+        /// The token RSA public key.
+        /// </summary>
+        /// <remarks>
+        /// In any other code repository I would store this in the appsetting.json file as the 
+        /// value "StoreInUserSecretsOrAzureWebServiceConfiguration", but this is a test example.
+        /// https://cryptotools.net/rsagen
+        /// </remarks>
+        public string PrivateKey { get; set; }
+
+        /// <summary>
+        /// The token RSA public key.
+        /// </summary>
+        /// <remarks>
+        /// In any other code repository I would store this in the appsetting.json file as the 
+        /// value "StoreInUserSecretsOrAzureWebServiceConfiguration", but this is a test example.
+        /// https://cryptotools.net/rsagen
+        /// </remarks>
+        public string PublicKey { get; set; }
+
+        /// <summary>
+        /// The token issuer.
+        /// </summary>
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// The token audience.
+        /// </summary>
+        public string Audience { get; set; }
+    }
+}
